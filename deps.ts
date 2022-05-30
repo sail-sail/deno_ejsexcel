181a549edc1013522c70414caa81d54e66f87184
Sail <151263555@qq.com>
2022-05-30 12:02:37 +0800

    first commit

--- deps.ts
+++ deps.ts
@@ -5,12 +5,10 @@ export { readFile } from "https://deno.land/std@0.141.0/node/fs/promises.ts";
 import path from "https://deno.land/std@0.141.0/node/path.ts";
 import * as crypto from "https://deno.land/std@0.141.0/node/crypto.ts"
 import { nextTick } from "https://deno.land/std@0.141.0/node/process.ts";
-import { qrcode } from "https://deno.land/x/qrcode@v2.0.0/mod.ts";
 
 export {
   zlib,
   path,
   crypto,
   nextTick,
-  qrcode,
 }

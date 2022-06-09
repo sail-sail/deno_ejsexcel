9756fff7cddae317fa3f4a9b44ef3e9d66bc5519
Sail <151263555@qq.com>
2022-06-09 15:35:54 +0800

    DOMParser

--- deps.ts
+++ deps.ts
@@ -5,12 +5,10 @@ export { readFile } from "https://deno.land/std/node/fs/promises.ts";
 import path from "https://deno.land/std/node/path.ts";
 import * as crypto from "https://deno.land/std/node/crypto.ts"
 import { nextTick } from "https://deno.land/std/node/process.ts";
-import { DOMParser } from "https://deno.land/x/deno_dom/deno-dom-wasm.ts";
 
 export {
   zlib,
   path,
   crypto,
   nextTick,
-  DOMParser,
 }

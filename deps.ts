6c2d4db028f85ee07de0ef4170b8cb2b548654dd
Sail <151263555@qq.com>
2023-02-27 09:49:01 +0800

    update deno

--- deps.ts
+++ deps.ts
@@ -1,9 +1,9 @@
-export { Buffer } from "node:buffer";
-import zlib from "node:zlib";
-export { Readable, Stream } from "node:stream";
+export { Buffer } from "https://deno.land/std@0.177.0/node/buffer.ts";
+import zlib from "https://deno.land/std@0.177.0/node/zlib.ts";
+export { Readable, Stream } from "https://deno.land/std@0.177.0/node/stream.ts";
 export { encode as encodeBase64 } from "https://deno.land/std@0.178.0/encoding/base64.ts";
-export { nextTick } from "node:process";
-import path from "node:path";
+export { nextTick } from "https://deno.land/std@0.177.0/node/process.ts";
+import path from "https://deno.land/std@0.177.0/node/path.ts";
 
 export {
   zlib,
